ce74843d0a23d7778f37239f23872c84e0eb5112
EngMohammed123 <mk2596292@gmail.com>
2024-06-10 18:58:13 +0300

    complete profile screen

--- lib/Feature/admin/presentation/widgets/admin_profile_screen_details.dart
+++ lib/Feature/admin/presentation/widgets/admin_profile_screen_details.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:school_room/Feature/admin/presentation/widgets/country_text_field.dart';
+import 'package:school_room/Feature/admin/presentation/widgets/email_text_field.dart';
+import 'package:school_room/Feature/admin/presentation/widgets/image_of_profile_widget.dart';
+import 'package:school_room/Feature/admin/presentation/widgets/name_text_field.dart';
+import 'package:school_room/Feature/admin/presentation/widgets/password_text_field.dart';
+import 'package:school_room/Feature/admin/presentation/widgets/phone_text_field.dart';
 
 class AdminProfileScreenDetails extends StatelessWidget{
   const AdminProfileScreenDetails({super.key});
@@ -21,18 +27,67 @@ class AdminProfileScreenDetails extends StatelessWidget{
             ),
             ),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.009,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/Rectangle111.png')
-              ],
-            )
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.003,
+              ),
+              const ImageOfProfile(),
+                Padding(
+                padding: const EdgeInsets.only(right: 16 , left: 16 , bottom: 22),
+                child: Column(
+                  children: [
+                    const NameTextField(),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.013,
+                    ),
+                    const EmailTextField(),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.013,
+                    ),
+                    const PasswordTextField(),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.013,
+                    ),
+                    const PhoneTextField(),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.013,
+                    ),
+                    const CountryTextField(),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  width: MediaQuery.of(context).size.width * 0.66,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                     textStyle: GoogleFonts.cairo(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w300
+                     ),
+                      backgroundColor: Color.fromARGB(255, 85, 188, 165),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      'حفظ التغييرات',
+                      style: GoogleFonts.cairo(
+                        fontSize: 20
+                      ),
+                      ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+              ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
        )
        );
--- lib/Feature/admin/presentation/widgets/index.dart
+++ lib/Feature/admin/presentation/widgets/index.dart
@@ -4,5 +4,11 @@ export 'admin_group_screen_details.dart';
 export 'admin_list_view_item.dart';
 export 'admin_profile_screen_details.dart';
 export 'admin_screen_details_widget.dart';
+export 'country_text_field.dart';
+export 'email_text_field.dart';
 export 'groups_list_view_item_widget.dart';
+export 'image_of_profile_widget.dart';
+export 'name_text_field.dart';
+export 'password_text_field.dart';
+export 'phone_text_field.dart';
 
